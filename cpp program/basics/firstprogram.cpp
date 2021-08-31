b5a425ad30ce00dfc7aaca3fdd5a9e78903919ad
unknown <sinharohit3333@gmail.com>
2021-08-31 11:42:30 +0530

    first program

---
+++ cpp program/basics/firstprogram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  int fav_num;
+  std::cout << "Enter your favorite number between 1 to 100";
+  std::cin >> fav_num;
+  std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+  return 0;
+}
